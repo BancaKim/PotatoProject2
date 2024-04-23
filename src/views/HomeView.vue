badeaf45e38ad46614dd270c8b74d34051711cd1
sangjoon88 <ramax@naver.com>
2024-04-23 13:57:03 +0900

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,35 @@
 <template>
-    <h1>마켓</h1>
-    <hr class="my-4">
-    <PostFilter v-model:title="params.title_like" v-model:limit="params._limit" />
-    <div class="d-flex" role="search">
-        <button class="btn btn-outline-success" type="button" @click="goPage">글쓰기</button>
-    </div>
-    <hr class="my-4">
+  <h1>마켓</h1>
+  <hr class="my-4">
+  <PostFilter v-model:title="params.title_like" v-model:limit="params._limit" />
+  <div class="d-flex" role="search">
+      <button class="btn btn-outline-success" type="button" @click="goPage">글쓰기</button>
+  </div>
+  <hr class="my-4">
 
-    <AppLoading v-if="loading" />
-    <AppError v-else-if="error" :message="'error.message'" />
-    <template v-else>
-        <AppGrid :items="posts">
-            <template v-slot="{ item }">
-                <AppCard :title="item.title" :content="item.content" :created-at="item.createdAt"
-                    @click="goPageId(item.id)" @modal="openModal(item)">
-                </AppCard>
-            </template>
-        </AppGrid>
-        <AppPagination :current-page="params._page" :pageCount="pageCount" @page="page => (params._page = page)" />
-    </template>
+  <AppLoading v-if="loading" />
+  <AppError v-else-if="error" :message="'error.message'" />
+  <template v-else>
+      <AppGrid :items="posts">
+          <template v-slot="{ item }">
+              <AppCard :title="item.title" :content="item.content" :created-at="item.createdAt"
+                  @click="goPageId(item.id)" @modal="openModal(item)">
+              </AppCard>
+          </template>
+      </AppGrid>
+      <AppPagination :current-page="params._page" :pageCount="pageCount" @page="page => (params._page = page)" />
+  </template>
 
-    <Teleport to="#modal">
-        <PostModal v-model="show" :title="modalTitle" :content="modalContent" :created-at="modalCreatedAt" />
-    </Teleport>
-    <!-- summary -->
-    <template v-if="posts&&posts.length>0">
-        <hr class="my-5">
-        <AppCard2>
-            <PostDetailView :id="posts[0].id"></PostDetailView>
-        </AppCard2>
-    </template>
+  <Teleport to="#modal">
+      <PostModal v-model="show" :title="modalTitle" :content="modalContent" :created-at="modalCreatedAt" />
+  </Teleport>
+  <!-- summary -->
+  <template v-if="posts&&posts.length>0">
+      <hr class="my-5">
+      <AppCard2>
+          <PostDetailView :id="posts[0].id"></PostDetailView>
+      </AppCard2>
+  </template>
 </template>
 
 <script setup>
@@ -50,11 +50,11 @@ const loading = ref(false);
 
 //정렬
 const params = ref({
-    _sort: 'createdAt',
-    _order: 'desc',
-    _page: 1,
-    _limit: 9,
-    title_like: '',
+  _sort: 'createdAt',
+  _order: 'desc',
+  _page: 1,
+  _limit: 9,
+  title_like: '',
 })
 
 //pagination
@@ -63,37 +63,37 @@ const pageCount = computed(()=> Math.ceil(totalCount.value/ params.value._limit)
 
 
 const fetchPosts = async() => {
-    try{
-        loading.value= true;
-        const { data,headers } = await getPosts(params.value);
-        posts.value = data;
-        totalCount.value = headers['x-total-count'];
-    }catch(err){
-        error.value = err;
-    }finally{
-        loading.value =false;
-    }
-    // getPosts()
-    // .then((response)=>{
-    //     console.log('response: ', response);
-    // }).catch(error=>{
-    //     console.log('error: ',error);
-    // });
+  try{
+      loading.value= true;
+      const { data,headers } = await getPosts(params.value);
+      posts.value = data;
+      totalCount.value = headers['x-total-count'];
+  }catch(err){
+      error.value = err;
+  }finally{
+      loading.value =false;
+  }
+  // getPosts()
+  // .then((response)=>{
+  //     console.log('response: ', response);
+  // }).catch(error=>{
+  //     console.log('error: ',error);
+  // });
 }
 
 
 const goPage = () => {
-    router.push('/posts/create');
+  router.push('/posts/create');
 };
 watchEffect(fetchPosts);
 const goPageId = (id) => {
-    // router.push(`/posts/'${id}`);
-    router.push({
-        name:'PostDetail',
-        params: {
-            id,
-        }
-    })
+  // router.push(`/posts/'${id}`);
+  router.push({
+      name:'PostDetail',
+      params: {
+          id,
+      }
+  })
 };
 const show = ref(false);
 const modalTitle = ref('');
@@ -101,10 +101,10 @@ const modalContent = ref('');
 const modalCreatedAt = ref('');
 
 const openModal= ({title, content, createdAt})=>{
-    show.value = true;
-    modalTitle.value =title;
-    modalContent.value =content;
-    modalCreatedAt.value =createdAt;
+  show.value = true;
+  modalTitle.value =title;
+  modalContent.value =content;
+  modalCreatedAt.value =createdAt;
 }
 
 </script>
