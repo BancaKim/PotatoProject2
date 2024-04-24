6ae0a895838617931c46d16ee3737efc3441b481
BancaKim <weberian92@gmail.com>
2024-04-24 09:25:08 +0900

    confilct solved

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,12 +9,21 @@
 
 <script>
 export default {
-  setup() {
-
-
-    return {}
+  data() {
+    return {
+    };
+  },
+mounted() {
+  if (window.kakao && window.kakao.maps) {
+    this.initMap();
+  } else {
+    const script = document.createElement('script');
+    script.onload = () => kakao.maps.load(this.initMap);
+    script.src =
+      'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=198fae9257e3a2b5af3086644a5de975';
+    document.head.appendChild(script);
   }
-}
+}}
 </script>
 
 <style lang="scss" scoped></style>
